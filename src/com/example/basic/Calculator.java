3c9b1560bf171e2d5a48ef7ef7828abe381ef5dc
sinyoung0403 <tlstjs753@gmail.com>
2025-02-27 16:38:11 +0900

    step02 - 클래스 적용해서 계산기 만들기 #feat num01, num02 속성부분 추가

--- src/com/example/basic/Calculator.java
+++ src/com/example/basic/Calculator.java
@@ -10,7 +10,10 @@ import java.util.Objects;
 public class Calculator {
   // 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
   // 속성 정의 > 인터페이스 리스트 형태로 받는게 낫다.
-  private List<Number> resultList = new ArrayList<Number>();;
+  private List<Number> resultList = new ArrayList<Number>();
+  // Lv2 에서는 정수값만을 받기를 원함.
+  private int num01;
+  private int num02;
   Number result;
 
   // Getter 함수. List 접근 가능
@@ -18,48 +21,57 @@ public class Calculator {
     return this.resultList;
   }
 
-  // Setter 함수. List 에 값을 추가할 수 있음.
-  public void setResultList(Number value) {
-    this.resultList.add(value);
+  // Getter 함수. 기존 입력 값 가져오기
+  public List<Number> getNum() {
+    return List.of(num01, num02);
+  }
+
+  // Setter 함수. 입력값 바꾸기.
+  public void setNum(int num01, int num02) {
+    this.num01 = num01;
+    this.num02 = num02;
   }
 
   /* 기능 정의*/
   // 값을 더하는 함수
-  int sum(int val1, int val2){
-    return val1 + val2;
+  public Number sum(){
+    return this.num01 + this.num02;
   }
+
   // 값을 빼는 함수
-  int subtract(int val1, int val2){
-    return val1 - val2;
+  public Number subtract(){
+    return this.num01 - this.num02;
   }
+
   // 값을 곱하는 함수
-  int multiply(int val1, int val2){
-    return val1 * val2;
+  public Number multiply(){
+    return this.num01 * this.num02;
   }
+
   // 값을 나누는 함수
-  double divide(int val1, int val2){
-    if (val2 == 0){
+  double divide(){
+    if (this.num02 == 0){
       throw new IllegalArgumentException("분모는 0이 될 수 없습니다.");
     }
     else {
-      return (double)val1 / (double) val2;
+      return (double)num01 / (double) num02;
     }
   }
 
   // 값 저장 클래스
-  public Number calculate(int firstInt, int secondInt, char symbol) {
+  public Number calculate(char symbol) {
     switch (symbol) {
       case '+':
-        result = sum(firstInt, secondInt);
+        result = sum();
         break;
       case '-':
-        result = subtract(firstInt, secondInt);
+        result = subtract();
         break;
       case '/':
-        result = divide(firstInt, secondInt);
+        result = divide();
         break;
       case '*':
-        result = multiply(firstInt, secondInt);
+        result = multiply();
         break;
       default:
         throw new IllegalArgumentException("올바른 연산 기호를 입력하세요. (+, -, *, /)");

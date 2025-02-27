a438092b7c02b73cdeb10011f95aee340494818a
sinyoung0403 <tlstjs753@gmail.com>
2025-02-27 16:38:30 +0900

    step02 - 클래스 적용해서 계산기 만들기 #feat setter 적용

--- src/com/example/basic/App.java
+++ src/com/example/basic/App.java
@@ -14,27 +14,23 @@ public class App {
       // 숫자 두 개 받기
       int firstInt = Input.getIntSc("첫 번째 숫자를 입력하세요. : ");
       int secondInt = Input.getIntSc("두 번째 숫자를 입력하세요. : ");
+      calculator.setNum(firstInt,secondInt);
 
       // 기호 입력 받기.
       char symbol = Input.getCharSc("사칙연산 기호를 입력하세요. : ");
 
       // 연산 결과를 calculator 에서 실행하기
-      Output.printOutput("결과 값: "+calculator.calculate(firstInt,secondInt,symbol));
+      Output.printOutput("입력 값: " + calculator.getNum());
+      Output.printOutput("결과 값: "+calculator.calculate(symbol));
       Output.printLineDivider();
 
       // 값의 삭제 및 추가 여부 받기.
       Output.printOutput("현재 저장되어 있는 값: " + calculator.getResultList() );
-      String deleteString = Input.getStringSc("( 엔터 입력 시 스킵 | C 입력 시 데이터 추가 | D 입력 시 가장 먼저 저장된 값 삭제)");
+      String deleteString = Input.getStringSc("( 엔터 입력 시 스킵 | D 입력 시 가장 먼저 저장된 값 삭제)");
 
       // 값을 삭제하고 싶을 시 removeResult() 함수 호출
       if(deleteString.equals("D")||deleteString.equals("d")) {
         calculator.removeResult();
-      }
-
-      // 값을 추가하고 싶을 시 setResultList() 함수 호출
-      else if (deleteString.equals("C")||deleteString.equals("c")) {
-        int setterInt = Input.getIntSc("추가할 값을 입력해주세요. : ");
-        calculator.setResultList(setterInt);
       }
       Output.printLineDivider();
 

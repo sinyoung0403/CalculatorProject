0f0ae64698130334b0725d1fafe3a629a83a4159
sinyoung0403 <tlstjs753@gmail.com>
2025-03-04 10:47:13 +0900

    step03 - 도전 계산기 만들기 #feat Main 연결

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    List<Object> list = new ArrayList<>(List.of(1,2));
+    System.out.println("list = " + list.get(0));
+    System.out.println("listsize =" + list.size());
+    list.remove(0);
+    System.out.println("list = " + list.get(0));
+    System.out.println("listsize =" + list.size());
+
   }
 }
---
+++ src/com/example/swing/App.java
@@ -0,0 +1,8 @@
+package com.example.swing;
+
+public class App {
+  // 실제 동작을 구현할 것이고.
+  public static void main(String[] args) {
+    new SwingUI();
+  }
+}

c9fc1bf38af338f85fa514c080daef5410c984a2
sinyoung0403 <tlstjs753@gmail.com>
2025-03-04 17:40:02 +0900

    step03 - 도전 계산기 만들기 #chore Main 파일 삭제

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main {
-  public static void main(String[] args) {
-    double a = 4.00;
-    int b = 4;
-    System.out.println(a+b);
-}}
